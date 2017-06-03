8f61e5bf2fb4ce26f38c991f91d4782d683db01d
lithium <code@hlidskialf.com>
2017-06-03 12:06:07 -0700

    Permission doesn’t have __unicode__

--- discoverer/admin.py
+++ discoverer/admin.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import Permission
 
 
 class PermissionAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'content_type', 'codename')
+    list_display = ('__str__', 'content_type', 'codename')
     list_filter = ('content_type',)
 admin.site.register(Permission, PermissionAdmin)
