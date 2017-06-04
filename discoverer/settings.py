17dbae91835baddc55c7468b011cb395271f78ed
lithium <code@hlidskialf.com>
2017-06-04 16:59:19 -0700

    redirect to index dont stream

--- discoverer/settings.py
+++ discoverer/settings.py
@@ -65,10 +65,9 @@ MIDDLEWARE = [
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ORIGIN_WHITELIST = (
-#     'www.ingress.com',
-# )
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    'www.ingress.com',
+)
 
 
 ROOT_URLCONF = 'discoverer.urls'
--- discoverer/views.py
+++ discoverer/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.http import StreamingHttpResponse, Http404, HttpResponse
 from django.utils.decorators import method_decorator
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 from discoverer.models import PortalIndex
 
@@ -14,18 +14,15 @@ class Home(TemplateView):
 
 
 @method_decorator(login_required, name='dispatch')
-class ServeIndex(PermissionRequiredMixin, View):
+class ServeIndex(PermissionRequiredMixin, RedirectView):
+    permanent = False
     permission_required = ('discoverer.read_portalindex',)
     raise_exception = True
     http_method_names = ('get',)
 
-    def get(self, request, *args, **kwargs):
+    def get_redirect_url(self, *args, **kwargs):
         idx = PortalIndex.objects.get_active()
         if idx is None:
             raise Http404
-        # response = StreamingHttpResponse(idx.indexfile.file, content_type='application/json')
-        response = HttpResponse(idx.indexfile.file, content_type='application/json')
-        response['Content-Length'] = idx.indexfile.size
-        # response['Content-Disposition'] = "attachment; filename={}".format(idx.name)
-        return response
+        return idx.indexfile.url
 
